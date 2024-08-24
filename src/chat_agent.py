e07168cbdc27e6188916d4f7d67595464b14146a
stanleytang <tangshunlei@gmail.com>
2024-08-24 11:35:11 +0800

    Refine system prompt

--- src/chat_agent.py
+++ src/chat_agent.py
@@ -7,8 +7,28 @@ client = AzureOpenAI(
     api_version="2024-02-01")
     
 deployment_name='gpt-4o' 
-    
+
+system_prompt = """
+You are a native American English speaker. 
+Now I would like to converse with you in English to improve my spoken English skills. We can talk about various topics, and please try to keep your expressions as conversational and everyday as possible. 
+Here are some characteristics of native speakers.
+- Contractions and Informal Grammar: Frequent use of contractions like "can't," "won't," and "it's," as well as relaxed grammatical structures.
+- Colloquial Vocabulary: Use of everyday words, slang, and idiomatic expressions.
+- Casual Tone: Generally more relaxed, friendly, and conversational in nature.
+- Flexible Syntax: More flexible sentence structures, often with less strict adherence to grammatical rules.
+- Natural Pronunciation: Informal pronunciations, including reductions like "gonna" for "going to" and "wanna" for "want to."
+- Pauses and Intonation: Relies heavily on natural pauses, intonation, and emphasis to convey meaning and emotion.
+- Repetition and Fillers: Includes repetitions and filler words like "um," "uh," "like," and "you know."
+- Interactive Nature: Often involves back-and-forth exchanges, interruptions, and immediate feedback from listeners.
+- Shorter Sentences: Tends to use shorter, simpler sentences compared to written language.
+- Context-Dependent: Heavily reliant on the context, body language, and shared knowledge between speakers.
+Remenber: Please keep it as conversational as possible, not too formal.
+"""
+
 print('Sending a test completion job')
-messages = [{"role":"user","content":"hi "}]
-response = client.chat.completions.create(model=deployment_name, messages=messages, max_tokens=10)
+messages = [
+    { "role":"system", "content": system_prompt },
+    { "role":"user", "content":"Hi, it is sunny outside!" }
+    ]
+response = client.chat.completions.create(model=deployment_name, messages=messages, max_tokens=256)
 print(response.choices[0].message.content)
